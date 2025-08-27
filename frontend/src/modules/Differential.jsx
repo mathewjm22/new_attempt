ebbe142909781d615a19dd4d9edc4333ee4df7c5
mathewjm22 <75663388+mathewjm22@users.noreply.github.com>
2025-08-26 22:20:40 -0600

    Create Differential.jsx

---
+++ frontend/src/modules/Differential.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Differential = () => {
+  return (
+    <div>
+      <h2>Differential</h2>
+      {/* Content for the Differential module will go here */}
+    </div>
+  );
+};
+
+export default Differential;
