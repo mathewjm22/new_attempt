dd04c0e6affc0f40ab9b50ed4a35cec17f566a2d
mathewjm22 <75663388+mathewjm22@users.noreply.github.com>
2025-08-26 19:14:44 -0600

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Clinical Trainer</h1>
+      </header>
+      <main>
+        <p>Welcome to Clinical Trainer!</p>
+        {/* Add your routes or components here */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
